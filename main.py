e9693d14a165481b6a8017e33aea7586388d8724
Momfer de Mol <93478412+momferdemol@users.noreply.github.com>
2023-04-27 19:51:24 +0200

    rock paper scissors game

--- main.py
+++ main.py
@@ -292,9 +292,9 @@ def who_pays_bill():
 
 def play_with_lists():
     # create a matrix of lists and ask for which position in the matrix the value needs to change.
-    row1 = ["x","x","x"]
-    row2 = ["x","x","x"]
-    row3 = ["x","x","x"]
+    row1 = ["X","X","X"]
+    row2 = ["X","X","X"]
+    row3 = ["X","X","X"]
 
     matrix = [row1, row2, row3]
 
@@ -308,6 +308,34 @@ def play_with_lists():
     matrix[row][column] = "M"
 
     print(f"\n{row1}\n{row2}\n{row3}\n")
+
+
+def rock_paper_scissors():
+
+    options = ["Rock", "Paper", "Scissors"]
+    user_choice = int(input("What do you choose? Type 1 for Rock, 2 for Paper, or 3 for Scissors.\n"))
+    
+    if user_choice < 1 or user_choice > 3:
+        print("\nWrong input. You suck!\n")
+    else:
+        print(f"\nYou\'ve chosen {options[user_choice - 1]}\n")
+        print("Computer chose...\n")
+        computer_choice = random.randint(1, 3)
+        print(f"Computer has chosen {options[computer_choice - 1]}\n")
+
+        # Rock wins against scissors
+        # Scissors win against paper
+        # Paper wins against rock
+        if user_choice == 1 and computer_choice == 3:
+            print("You win!\n")
+        elif user_choice == 3 and computer_choice == 2:
+            print("You win!\n")
+        elif user_choice == 2 and computer_choice == 1:
+            print("You win!\n")
+        elif user_choice == computer_choice:
+            print("Let\'s try again.\n")
+        else:
+            print("You lose.\n")
 
 
 # length_user_input()
@@ -325,3 +353,4 @@ def play_with_lists():
 # heads_or_tails()
 # who_pays_bill()
 # play_with_lists()
+rock_paper_scissors()
