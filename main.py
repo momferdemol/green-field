2152923a956803eaa1ff400b5d4d67ed296ac7f4
Momfer de Mol <93478412+momferdemol@users.noreply.github.com>
2023-04-28 14:43:19 +0200

    highest score loop

--- main.py
+++ main.py
@@ -353,9 +353,41 @@ def average_height():
         student_heigths[height] = int(student_heigths[height])
         heights_total += student_heigths[height]
 
+    # can also use sum() to calculate list total
     heights_average = round(heights_total / (height + 1))
     print(f"\nYour input: {student_heigths}\n")
     print(f"The average height is: {heights_average}\n")
+
+
+def highest_score():
+
+    score_input = input("Input a list of student scores, separated by a space.\n").split()
+
+    for score in range(0, len(score_input)):
+        score_input[score] = int(score_input[score])
+
+    for score in range(0, len(score_input)):
+        a = score_input[score]
+        # print(f"a: {a}")
+
+        # can also use max() to find highest value in list
+        if score != len(score_input) - 1:
+            b = score_input[score + 1]
+            # print(f"b: {b}")
+
+            if a > b:
+                high_score = a
+    
+    print(f"\nHighest score: {high_score}\n")
+
+    # and now much simpler :-)
+
+    highest_score = 0
+    for score in score_input:
+        if score > highest_score:
+            highest_score = score
+    
+    print(f"Highest score: {high_score}\n")
 
 
 # length_user_input()
@@ -375,3 +407,4 @@ def average_height():
 # play_with_lists()
 # rock_paper_scissors()
 # average_height()
+# highest_score()
