70a74819f292f940375eca4e618eb048511a6972
Momfer de Mol <93478412+momferdemol@users.noreply.github.com>
2023-04-28 16:28:20 +0200

    password generator

--- main.py
+++ main.py
@@ -420,11 +420,33 @@ def password_generator():
     tprint("password generator")
     print("Welcome to the password generator!")
     
-    number_of_digits = input("How many digits for you password? e.g. 12, 14, 20\n")
-    number_of_numbers = input("How many numbers do you want to include?\n")
-    number_of_symbols = input("How many symbols do you want to include?\n")
+    # ask for input from user
+    number_for_length = int(input("What should be the length of your password? e.g. 12, 14, 20\n"))
+    number_of_numbers = int(input("How many numbers do you want to include?\n"))
+    number_of_symbols = int(input("How many symbols do you want to include?\n"))
+    
+    # subtracks numbers and symbols for total password digits
+    number_for_length -= number_of_numbers + number_of_symbols
 
+    # generate lists with random letters, numbers, and symbols
+    list_of_letters = random.choices(letters, k=number_for_length)
+    list_of_numbers = random.choices(numbers, k=number_of_numbers)
+    list_of_symbols = random.choices(symbols, k=number_of_symbols) 
+    
+    # combine separate lists together
+    password_list = list_of_letters + list_of_numbers + list_of_symbols
 
+    # shuffle all characters in the list
+    random.shuffle(password_list)
+
+    # set empty string and loop through list to make one string value
+    password = ""
+    for item in password_list:
+        password += item
+    
+    print(password)
+
+    
 # length_user_input()
 # switch_variables()
 # hello_world()
@@ -443,4 +465,5 @@ def password_generator():
 # rock_paper_scissors()
 # average_height()
 # highest_score()
-fizz_buzz()
+#fizz_buzz()
+# password_generator()
